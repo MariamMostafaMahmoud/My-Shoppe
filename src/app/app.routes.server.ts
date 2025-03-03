619c8ee266040d6aa0eea849704cfafbe8e76aa1
Mariam Mostafa Mahmoud <mariam259mostafa@gmail.com>
2025-03-04 01:25:54 +0200

    forbuild

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,17 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
-  }
+  },
+  {
+    path: 'brandDetails/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Client
+  },
 ];
